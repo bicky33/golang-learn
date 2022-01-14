f40575996dc7a70f4fb1afd0f052ff92f7967b7b
Biki <biki.hamzah@gmail.com>
2022-01-14 13:05:31 +0700

    looping

--- hello.go
+++ hello.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"looping"
+)
 
 func main(){
 	fmt.Println("Hello World")
---
+++ looping.go
@@ -0,0 +1,9 @@
+package looping 
+
+import "fmt"
+
+func looping() {
+	for i := 0; i < 5; i++ {
+		fmt.Println("iterasi: ", i)
+	}
+}
